cc36331a7dce7e30299976c12f1ab4d2e6d304e1
Mikey <karthikalaivani18@gmail.com>
2026-08-10 21:48:38 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAg_-1Zvv_vluxPmfd2s2m2RMX5CjAXr7M",
+  authDomain: "pondguardian-b4808.firebaseapp.com",
+  projectId: "pondguardian-b4808",
+  storageBucket: "pondguardian-b4808.firebasestorage.app",
+  messagingSenderId: "756994004617",
+  appId: "1:756994004617:web:74df8a68de9e9b8e825858",
+  measurementId: "G-N3Y1HZN33S"
+};
